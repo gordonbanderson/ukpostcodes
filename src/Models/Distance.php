c1eb7dde34a10d0d7ec5545e2bb9676afdb685b8
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-05-08 18:29:20 +0100

    ENHANCEMENT: Distance unit constants

---
+++ src/Models/Distance.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Suilven\UKPostCodes\Models;
+
+/**
+ * Class Distance
+ * @package Suilven\UKPostCodes\Models
+ */
+class Distance
+{
+    /** @var string constant to denote distance is in miles */
+    const MILES='M';
+
+    /** @var string constant to denote distance is in nautical miles */
+    const NAUTICAL_MILES='N';
+
+    /** @var string constant to denote distance is in km */
+    const KM='K';
+}
