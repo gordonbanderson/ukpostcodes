731c3c9fe78eb3b5f87eba6d3b7ca59b462722c8
Gordon Anderson <gordon.b.anderson@gmail.com>
2020-05-09 22:28:43 +0100

    ENHANCEMENT: Using postcodes.io documentation, typed most of the properties correctly

--- src/Models/PostCode.php
+++ src/Models/PostCode.php
@@ -8,31 +8,41 @@ use _HumbugBox7c6aed0dbb3c\Roave\BetterReflection\Reflection\Adapter\ReflectionO
  * Class PostCode
  * @package Suilven\UKPostCodes\Models
  *
- * * @property int $admin_county
-* @property int $admin_district
-* @property int $admin_ward
-* @property int $ccg
-* @property int $ced
-* @property int $codes
-* @property int $country
-* @property int $eastings
-* @property int $european_electoral_region
-* @property int $incode
-* @property int $latitude
-* @property int $longitude
-* @property int $lsoa
-* @property int $msoa
-* @property int $nhs_ha
-* @property int $northings
-* @property int $nuts
-* @property int $outcode
-* @property int $parish
-* @property int $parliamentary_constituency
-* @property int $postcode
-* @property int $primary_care_trust
-* @property int $quality
-* @property int $region
-
+ * @property string|null $admin_county
+ * @property string|null $admin_district
+ * @property string|null $admin_ward
+ * @property string|null $ccg
+ * @property string|null $ced
+ *
+ * @todo Review this one
+ * @property string|null $codes
+ *
+ * @property string $country
+ * @property int|null $eastings
+ * @property string|null $european_electoral_region
+ * @property string $incode
+ * @property float|null $latitude
+ * @property float|null $longitude
+ * @property int $lsoa
+ * @property int $msoa
+ * @property string|null $nhs_ha
+ * @property int|null $northings
+ * @property string|null $nuts
+ * @property string $outcode
+ * @property string|null $parish
+ * @property string|null $parliamentary_constituency
+ * @property string $postcode
+ * @property string|null $primary_care_trust
+ * @property int $quality
+ * @property string|null $region
+ *
+ * Distance is returned by some but not all API endpoints
+ * @property float|null $distance
+ *
+ * For terminated postcodes
+ * @property month_terminated int|null
+ * @property year_terminated int|null
+ * @property terminated bool|null
  */
 class PostCode
 {
